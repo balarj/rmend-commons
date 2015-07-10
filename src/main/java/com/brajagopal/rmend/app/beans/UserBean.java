3531f0981b10f0aa32e5bc13c19dbe2815502cb0
Bala Rajagopal <bxr4261@rit.edu>
2015-07-10 04:32:51 -0700

    Refactored the UserBean - added functionality to generate UUID for users.

--- src/main/java/com/brajagopal/rmend/app/beans/UserBean.java
+++ src/main/java/com/brajagopal/rmend/app/beans/UserBean.java
@@ -1,20 +1,27 @@
 package com.brajagopal.rmend.app.beans;
 
+import com.google.gson.*;
 import org.apache.commons.lang3.RandomUtils;
 import org.joda.time.DateTime;
+
+import java.lang.reflect.Type;
+import java.util.UUID;
 
 /**
  * @author <bxr4261>
  */
+@SuppressWarnings("unused")
 public class UserBean {
 
     private static final long BASE_MODIFIER = (System.currentTimeMillis() - new DateTime("2015-07-07").getMillis());
     private Long uid;
     private String userName;
+    private String uuid;
 
-    private UserBean(Long _uid, String _userName) {
+    private UserBean(Long _uid, String _userName, String _uuid) {
         this.uid = _uid;
         this.userName = _userName;
+        this.uuid = _uuid;
     }
 
     public Long getUid() {
@@ -25,8 +32,12 @@ public class UserBean {
         return userName;
     }
 
+    public String getUuid() {
+        return uuid;
+    }
+
     public static UserBean create(String userName) {
-        return new UserBean((BASE_MODIFIER +  RandomUtils.nextLong(1, 200)), userName);
+        return new UserBean((BASE_MODIFIER +  RandomUtils.nextLong(1, 200)), userName, UUIDGenerator.generateUUID());
     }
 
     @Override
@@ -35,5 +46,36 @@ public class UserBean {
                 "uid=" + uid +
                 ", userName='" + userName + '\'' +
                 '}';
+    }
+
+    private static class UUIDGenerator {
+        /**
+         * Build a UUID.
+         */
+        public static String generateUUID() {
+            return UUID.randomUUID().toString();
+        }
+    }
+
+    public static class UserBeanSerDe implements JsonSerializer<UserBean>, JsonDeserializer<UserBean> {
+
+        @Override
+        public JsonElement serialize(UserBean userBean, Type type, JsonSerializationContext jsonSerializationContext) {
+            JsonObject root = new JsonObject();
+            root.addProperty("uid", userBean.getUid());
+            root.addProperty("userName", userBean.userName);
+            root.addProperty("uuid", userBean.uuid);
+            return root;
+        }
+
+        @Override
+        public UserBean deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+            final JsonObject root = jsonElement.getAsJsonObject();
+            final Long uid = root.get("uid").getAsLong();
+            final String userName = root.get("userName").getAsString();
+            final String uuid = root.get("uuid").getAsString();
+
+            return new UserBean(uid, userName, uuid);
+        }
     }
 }
